042942d4a584bd895f065d97b1e2b709a15ba2b6
Gabriela Wójcik <gabriela.wojcik@enova.pl>
2016-03-08 11:03:20 +0100

    Założenie projektu

---
+++ LoginListener/AfterLoginTestUI.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using Soneta.Business;
+using Soneta.Business.App;
+using Soneta.Types;
+using Soneta.Business.UI;
+using Soneta.AfterLoginTestUI;
+
+// rejestracja serwisu implementującego interfejs ILoginListenerUI
+// dla wszystkich zarejestrowanych w ten sposób serwisów zostanie podczas logowania zawołana metoda AfterLoginResult
+[assembly: Service(typeof(ILoginListenerUI), typeof(Soneta.AfterLoginTestUI.Registration))]
+
+
+namespace Soneta.AfterLoginTestUI
+{
+    // Obiekt klasy AfterLoginTestUI zostanie dodany do listy obiektów wyświetlanych po zalogowaniu
+    // Ponieważ klasa AfterLoginTestUI implementuje interfejs IVerifiable, możemy zweryfikować poprawność wpisanych danych
+    // Weryfikacja polega na porównaniu wpisanej wartości Control z losowo ustaloną wartością Pattern
+    [DataFormStyle(UseDialog = true)]
+    [Caption("Weryfikacja danych logowania")]
+    public class AfterLoginTestUI : IVerifiable
+    {
+        // "techniczna" deklaracja zmiennej Currency - ze względu na formularze (pageform.xml) dodatek musi posiadać referencję do Soneta.Types
+        Soneta.Types.Currency c1 = Soneta.Types.Currency.Zero;
+
+        string pattern;
+        string control;
+        Context context;
+
+        public AfterLoginTestUI(Context cx)
+        {
+            this.context = cx;
+
+            // Dodajemy weryfikator do listy weryfikatorów w sesji
+            // Na akceptacji okna weryfikator zostanie aktywowany - zostanie sprawdzone, czy wartość została poprawnie wpisana
+            this.context.Session.Verifiers.Add(new PoleVerifier(this));
+
+            // jako wartość wzorcową losujemy liczbę z zakresu 1-1000
+            pattern = (new Random().Next() % 1000 + 1).ToString();
+        }
+
+        //właściwość do zmiennej "pattern"
+        public string Pattern
+        {
+            get
+            {
+                return pattern;
+            }
+        }
+        //właściwość do zmiennej "control" 
+        public string Control
+        {
+            get
+            {
+                return control;
+            }
+            set
+            {
+                control = value;
+                this.context.Session.InvokeChanged();
+            }
+        }
+
+        // Interfejs IVerifiable wymaga implementacji metody GetVerifiers zwracającej kolekcję weryfikatorów
+        public IEnumerable GetVerifiers()
+        {
+            // rzutowanie Context.Session na obiekt IVerifiable i zwrócenie weryfikatorów zarejestrowanych w sesji
+            return ((IVerifiable)this.context.Session).GetVerifiers();
+        }
+
+        // Klasa weryfikatora, który będzie sprawdzany w teście
+        // klasa PoleVerifier dziedziczy po klasie Verifier
+        public class PoleVerifier : Verifier
+        {
+
+            private readonly AfterLoginTestUI afterLoginTestUI;
+
+            public PoleVerifier(AfterLoginTestUI afterLoginTestUI)
+            {
+                this.afterLoginTestUI = afterLoginTestUI;
+            }
+
+            // implementacja właściwości z klasy bazowej - zwracany komunikat błędu
+            public override string Description
+            {
+                get
+                {
+                    return String.Format("Wartość kontrolna nie zgadza się ze wzorcem.\nWartość oczekiwana: {0}\nWartość wpisana: {1}", afterLoginTestUI.Pattern, afterLoginTestUI.Control);
+                }
+            }
+
+            // implementacja właściwości z klasy bazowej - zwracany obiekt afterLoginTestUI
+            public override object Source
+            {
+                get
+                {
+                    return afterLoginTestUI;
+                }
+            }
+
+            // Wynik metody wskazuje, czy wpisane dane są poprawne
+            // Metoda zwraca true jeśli wpisana wartość kontrolna jest zgodna ze wzorcem
+            protected override bool IsValid()
+            {
+                return (afterLoginTestUI.Control == afterLoginTestUI.Pattern);
+            }
+
+            // Wynik metody wskazuje, czy należy przeprowadzić weryfikację
+            // Weryfikację przeprowadzamy dopiero na kliknięciu OK na formularzu, czyli na akcji AcceptRow
+            protected override bool IsAccept(object data, string property, VerifierAction action)
+            {
+                return action == VerifierAction.AcceptRow;
+            }
+
+        }
+    }
+
+    // klasa Registration implementuje intefejs ILoginListenerUI
+    // podczas logowania zostanie zawołana metoda AfterLoginResult dla tej klasy
+    public class Registration : ILoginListenerUI
+    {
+        // implementacja metody z interfejsu ILoginListenerUI
+        // przekazany obiekt klasy AfterLoginResultArgs zawiera listę obiektów, które zostaną otwarte po zalogowaniu
+        // dodanie do listy obiektu klasy AfterLoginTestUI oznacza, 
+        // że po zalogowaniu system otworzy odpowiedni formularz dla tego obiektu (AfterLoginTestUI.General.pageform.xml)
+        void ILoginListenerUI.AfterLoginResult(AfterLoginResultArgs args)
+        {
+            //dopisanie obiektu klasy AfterLoginTestUI do odpowiedniego kontera wraz z inicjalizacją pola Context
+            args.Values.Add(new AfterLoginTestUI(args.Context));
+        }
+    }
+}
---
+++ LoginListener/AfterLoginTestUISample.cs
@@ -0,0 +1,46 @@
+﻿using Soneta.Business;
+using Soneta.Business.App;
+using Soneta.Business.UI.Test;
+
+// rejestracja serwisu implementującego interfejs ILoginListenerUI
+// dla wszystkich zarejestrowanych w ten sposób serwisów zostanie podczas logowania zawołana metoda AfterLoginResult
+[assembly: Service(typeof(ILoginListenerUI), typeof(Soneta.AfterLoginTestUISample.Registration), Priority = 200)]
+
+namespace Soneta.AfterLoginTestUISample
+{
+    // obiekt klasy AfterLoginTestUISample zostanie wyświetlony po zalogowaniu do systemu
+    //prosty obiekt dla którego będzie wyświetlana odpowiednia informacja
+    public class AfterLoginTestUISample 
+    {
+        [Context]
+        //to jest autmoatyczna właściwość pozwalająca na przekazanie bieżącego Context-u 
+        public Context Context { get; set; }
+
+        public string Komunikat
+        {
+            get { return "Przykładowy komunikat po zalogowaniu"; }
+        }
+    }
+    // klasa Registration implementuje intefejs ILoginListenerUI
+    // podczas logowania zostanie zawołana metoda AfterLoginResult dla tej klasy
+    public class Registration : ILoginListenerUI
+    {
+        // "techniczna" deklaracja zmiennej 
+        // ze względu na formularze (pageform.xml) dodatek musi posiadać referencję do Soneta.Types
+        Soneta.Types.Currency c1 = Soneta.Types.Currency.Zero;
+
+        // implementacja metody z interfejsu ILoginListenerUI
+        // przekazany obiekt klasy AfterLoginResultArgs zawiera listę obiektów, które zostaną otwarte po zalogowaniu
+        // dodanie do listy obiektu klasy AfterLoginTestUISample oznacza, 
+        // że po zalogowaniu system otworzy odpowiedni formularz dla tego obiektu (AfterLoginTestUISample.General.pageform.xml)
+        void ILoginListenerUI.AfterLoginResult(AfterLoginResultArgs args)
+        {
+            //warunek, jeżeli użytkownikiem jest administrator zostanie dla niego wyświetlony komunikat
+            if (args.Login.OperatorName == "Administrator")
+                args.Values.Add(new AfterLoginTestUISample());
+        }
+    }
+}
+
+
+
